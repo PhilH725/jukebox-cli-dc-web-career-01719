c36e9ff340800e3bcd333429238ddcf134ea5f39
Phil Hartley <philh725@gmail.com>
2018-12-31 01:42:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   song_choice = gets.chomp
   
   if songs[song_choice.to_i]
-    puts "Playing #{songs[song_choice]}"
+    puts "Playing #{songs[song_choice.to_i]}"
   end
   
 end
