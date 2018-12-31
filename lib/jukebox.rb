0fb347981d8053da027e03ab34c200f971628842
Phil Hartley <philh725@gmail.com>
2018-12-31 02:12:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,6 +28,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.chomp
   
+  if songs.include?(song_choice)
+    puts "Playing #{songs[song_choice.to_i]}"
   if songs[song_choice.to_i]
     puts "Playing #{songs[song_choice.to_i]}"
   else
